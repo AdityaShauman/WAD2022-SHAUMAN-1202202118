d1f8a2b020910b563558d338a605e192aa1ac1ec
AdityaShauman <aditya.shauman@gmail.com>
2022-11-20 21:30:35 +0700

    v3

--- MODUL3 SHAUMAN/config/connector.php
+++ MODUL3 SHAUMAN/config/connector.php
@@ -3,7 +3,7 @@
 $connect = mysqli_connect("127.0.0.1:8080", "root", "", "showroom_shauman_table");
 
     function querydata($query) {
-    global $conn;
+    global $connect;
     $result = mysqli_query($connect, $query);
     $rows = [];
     while ( $row = mysqli_fetch_assoc($result)) {
