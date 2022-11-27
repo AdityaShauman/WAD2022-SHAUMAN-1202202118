958c3623a979eda795da92b22cb1b51bc4b18adb
AdityaShauman <aditya.shauman@gmail.com>
2022-11-27 11:01:59 +0700

    Ganti nama database

--- MODUL3 SHAUMAN/config/connector.php
+++ MODUL3 SHAUMAN/config/connector.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("localhost".":"."3306", "root", "", "showroom_shauman_table");
+$connect = mysqli_connect("localhost".":"."3306", "root", "", "modul3");
 
     function querydata($query) {
         global $connect;
